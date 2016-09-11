3fb745854eff0eb1de4980f3b9834d2abee74ded
rektide <rektide@voodoowarez.com>
2016-09-10 21:26:47 -0400

    eventLog extensions onto openzwave-shared.

--- prom-zwave.js
+++ prom-zwave.js
@@ -2,50 +2,67 @@ var
   OpenZwave= require("openzwave-shared"),
   util= require("util")
 
-function PromZwave( path, options){
-	if(!( this instanceof PromZwave)){
-		return new PromZwave( path, options)
+function factory( path, options){
+	if(this instanceof factory){
+		throw new Error("Invalid use of 'new'")
 	}
 	if( typeof path=== "object"){
 		options= path
 		path= undefined
 	}
-	if( !path){
-		path= "/dev/ttyUSB0"
-	}
-	OpenZwave.call(this, path, options)
-	return this
+	path= path|| "/dev/ttyUSB0"
+	options= options|| {}
+
+	var zwave= new OpenZwave(options)
+	Object.setPrototypeOf(zwave, PromZwave.prototype)
+	zwave.path= path
+	return zwave
 }
+
+function PromZwave(){}
 util.inherits(PromZwave, OpenZwave)
 
 PromZwave.prototype.eventLog= function(eventNames){
 	var logHandler= (eventName)=> {
-		return ()=> {
-			var args= Array.prototype.slice.call(0, arguments, this.logDepth+1)
+		return (...args)=> {
+			var i= this.logDepth;
+			for(; i>0&& !args[i-1]; --i){}
+			args= args.slice(0, i)
+			args.unshift(eventName)
+			console.log.apply(console, args)
 		}
 	}
-	['driver ready',
+	var listen= (eventName)=> this.on( eventName, logHandler( eventName));
+	[
+		'driver ready',
 		'driver failed',
 		'scan complete',
 		'node added',
+		'node naming',
 		'node available',
-		'node event'
-	].forEach(function(eventName){
-		this.on(eventName, logHandler(eventName))
-	})
+		'node ready',
+		'node event',
+		'polling enabled/disabled',
+		'scene event'
+	].forEach( listen)
 }
-
 PromZwave.prototype.logDepth= 1
 
+var _connect= OpenZwave.prototype.connect
+PromZwave.prototype.connect= function(path){
+	_connect.call( this, path|| this.path)
+}
+
 function main(){
-	var zwave= PromZwave(process.argv[2])
+	var zwave= module.exports(process.argv[2], {
+		ConsoleOutput: false
+	})
 	zwave.eventLog()
 	zwave.connect()
 	return zwave
 }
 
-module.exports= PromZwave
-module.exports.PromZwave= PromZwave
+module.exports= factory
 module.exports.main= main
 
 if(require.main=== module){
